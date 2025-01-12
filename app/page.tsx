fcbeeb1a0b029668dea39b2e439fc498f901ec41
river <riverehill@gmail.com>
2025-01-12 05:57:58 -0800

    Fix main.js link

--- app/page.tsx
+++ app/page.tsx
@@ -405,7 +405,7 @@ export default function Home() {
       <Script src='https://cdnjs.cloudflare.com/ajax/libs/iconify/3.1.1/iconify.min.js' />
       <Script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' />
       {/* <script src='main.js'></script> */}
-      <Script src='main.js' />
+      <Script src='./main.js' />
     </div>
   )
 }
