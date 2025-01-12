9426e95f6b9549d0e5a5c112ae6617ded864d66b
river <riverehill@gmail.com>
2025-01-12 06:38:01 -0800

    Replace hero background

--- app/page.tsx
+++ app/page.tsx
@@ -403,9 +403,8 @@ export default function Home() {
       </footer>
 
       <Script src='https://cdnjs.cloudflare.com/ajax/libs/iconify/3.1.1/iconify.min.js' />
-      <Script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' />
-      {/* <script src='main.js'></script> */}
-      <Script src='./main.js' />
+      {/* <Script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' /> */}
+      {/* <Script src='./main.js' /> */}
     </div>
   )
 }
