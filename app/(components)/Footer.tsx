5142120a9efebea657cfbdae8c578aac256e349a
river <riverehill@gmail.com>
2025-01-15 04:31:04 -0800

    Fix back to top button

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -1,25 +1,34 @@
+import { FaGithub } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+
 const Footer = () => {
   return (
-    <footer>
-      <div className='header'>{/* <!-- <h2>Contact</h2> --> */}</div>
+    <footer className='mt-8'>
       <section id='contact'>
-        {/* <!-- <form action="https://formsubmit.co/riverehill@gmail.com" method="POST">
-    <input type="text" name="name" placeholder="Name" required>
-    <input type="email" name="email" placeholder="Email" required>
-    <textarea name="message" placeholder="Message" style="height: 220px; overflow-y: hidden;" required></textarea>
-    <button type="submit">Send</button>
-  </form>  --> */}
         <div className='links'>
           <p>Get in touch</p>
-          <div className='icons'>
-            <a href='https://github.com/rivereh' target='_blank'>
-              <i className='fa-brands fa-github'></i>
+          <div className='flex justify-center text-4xl'>
+            <a
+              href='https://github.com/rivereh'
+              target='_blank'
+              className='footer-icon'
+            >
+              <FaGithub />
             </a>
-            <a href='http://linkedin.com/in/rivereh' target='_blank'>
-              <i className='fa-brands fa-linkedin'></i>
+            <a
+              href='http://linkedin.com/in/rivereh'
+              target='_blank'
+              className='footer-icon'
+            >
+              <FaLinkedin />
             </a>
-            <a href='mailto:riverehill@gmail.com' target='_blank'>
-              <i className='fa-solid fa-envelope'></i>
+            <a
+              href='mailto:riverehill@gmail.com'
+              target='_blank'
+              className='footer-icon'
+            >
+              <MdEmail />
             </a>
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,19 @@
-import Script from 'next/script'
 import ProjectSection from './(components)/ProjectSection'
 import Footer from './(components)/Footer'
 import Hero from './(components)/Hero'
 import AboutSection from './(components)/AboutSection'
+import SkillSection from './(components)/SkillSection'
+import BackToTopButton from './(components)/BackToTopButton'
 
 export default function Home() {
   return (
     <div>
-      <link
-        rel='stylesheet'
-        href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
-      />
-
-      {/* <!-- BACK TO TOP BUTTON --> */}
-      <button id='back-to-top'>
-        <i className='fas fa-arrow-up'></i>
-      </button>
-
-      <div id='jsi-rain-container' className='rain-container'></div>
-
       <Hero />
       <AboutSection />
-
-      {/* <!-- SKILLS --> */}
-      <section className='skills'>
-        <div className='header'>
-          <h2>My Skills</h2>
-        </div>
-        <div className='container'>
-          <div className='skill'>
-            <span className='iconify' data-icon='skill-icons:react-dark'></span>
-            <p>React</p>
-          </div>
-
-          <div className='skill'>
-            <span className='iconify' data-icon='devicon:typescript'></span>
-            <p>TypeScript</p>
-          </div>
-          <div className='skill'>
-            <span
-              className='iconify'
-              data-icon='vscode-icons:file-type-node'
-            ></span>
-            <p>Node</p>
-          </div>
-          <div className='skill'>
-            <span
-              className='iconify'
-              data-icon='skill-icons:expressjs-light'
-            ></span>
-            <p>Express</p>
-          </div>
-          <div className='skill'>
-            <span
-              className='iconify'
-              data-icon='vscode-icons:file-type-mongo'
-            ></span>
-            <p>MongoDB</p>
-          </div>
-
-          <div className='skill'>
-            <span className='iconify' data-icon='logos:python'></span>
-            <p>Python</p>
-          </div>
-          <div className='skill'>
-            <span className='iconify' data-icon='skill-icons:unity-dark'></span>
-            <p>Unity</p>
-          </div>
-          <div className='skill'>
-            <span className='iconify' data-icon='logos:c-sharp'></span>
-            <p>C#</p>
-          </div>
-          <div className='skill'>
-            <span className='iconify' data-icon='devicon:c'></span>
-            <p>C++</p>
-          </div>
-          <div className='skill'>
-            <span
-              className='iconify'
-              data-icon='vscode-icons:file-type-sql'
-            ></span>
-            <p>SQL</p>
-          </div>
-          <div className='skill'>
-            <span className='iconify' data-icon='logos:firebase'></span>
-            <p>Firebase</p>
-          </div>
-          <div className='skill'>
-            <span className='iconify' data-icon='skill-icons:aws-dark'></span>
-            <p>AWS</p>
-          </div>
-        </div>
-      </section>
-
+      <SkillSection />
       <ProjectSection />
-
+      <BackToTopButton />
       <Footer />
-      <Script src='https://cdnjs.cloudflare.com/ajax/libs/iconify/3.1.1/iconify.min.js' />
     </div>
   )
 }
