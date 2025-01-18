90250e1ff9cd5b12a0b8fa399a45b6e50c80766c
river <riverehill@gmail.com>
2025-01-17 17:12:17 -0800

    Remove react-focus-on

--- app/(components)/ProjectInfo.tsx
+++ app/(components)/ProjectInfo.tsx
@@ -4,7 +4,6 @@ import Slider from 'react-slick'
 import { Project } from '../(types)/Project.types'
 import { Icon } from '@iconify/react'
 import Image from 'next/image'
-import { FocusOn } from 'react-focus-on'
 
 type Props = {
   closeInfo: () => void
@@ -24,58 +23,56 @@ const ProjectInfo = ({ closeInfo, project }: Props) => {
   }
 
   return (
-    <FocusOn onEscapeKey={closeInfo}>
-      <div className='fixed left-0 top-0 flex h-screen w-full flex-col items-center justify-center gap-4 bg-black bg-opacity-50 py-4 text-white'>
-        <div className='shadow-l mt-6 flex h-[80%] max-w-[800px] flex-col gap-4 overflow-y-auto rounded-lg bg-neutral-800 p-3'>
-          {project.sliderImages ? (
-            <Slider {...settings} className='mb-6 max-h-[400px]'>
-              {project.sliderImages.map((imageUrl) => {
+    <div className='fixed left-0 top-0 flex h-screen w-full flex-col items-center justify-center gap-4 bg-black bg-opacity-50 py-4 text-white'>
+      <div className='shadow-l mt-6 flex h-[80%] max-w-[800px] flex-col gap-4 overflow-y-auto rounded-lg bg-neutral-800 p-3'>
+        {project.sliderImages ? (
+          <Slider {...settings} className='mb-6 max-h-[400px]'>
+            {project.sliderImages.map((imageUrl) => {
+              return (
+                <div key={project.id}>
+                  <Image
+                    className='max-h-[400px] w-full object-cover object-top'
+                    src={imageUrl}
+                    alt=''
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    style={{ width: '100%', height: 'auto' }}
+                  />
+                </div>
+              )
+            })}
+          </Slider>
+        ) : (
+          <div>
+            <img
+              className='max-h-[400px] w-full object-cover object-top'
+              src={project.coverImage}
+              alt=''
+            />
+          </div>
+        )}
+        <h2 className='text-center'>{project.title}</h2>
+        <p className=''>{project.desc}</p>
+        {project.techIcons && (
+          <div className='my-4'>
+            <p className='text-md mb-4 text-center font-bold'>Tech Stack</p>
+            <div className='flex justify-center gap-2 text-3xl'>
+              {project.techIcons.map((iconUrl) => {
                 return (
                   <div key={project.id}>
-                    <Image
-                      className='max-h-[400px] w-full object-cover object-top'
-                      src={imageUrl}
-                      alt=''
-                      width={0}
-                      height={0}
-                      sizes='100vw'
-                      style={{ width: '100%', height: 'auto' }}
-                    />
+                    <Icon icon={iconUrl} />
                   </div>
                 )
               })}
-            </Slider>
-          ) : (
-            <div>
-              <img
-                className='max-h-[400px] w-full object-cover object-top'
-                src={project.coverImage}
-                alt=''
-              />
             </div>
-          )}
-          <h2 className='text-center'>{project.title}</h2>
-          <p className=''>{project.desc}</p>
-          {project.techIcons && (
-            <div className='my-4'>
-              <p className='text-md mb-4 text-center font-bold'>Tech Stack</p>
-              <div className='flex justify-center gap-2 text-3xl'>
-                {project.techIcons.map((iconUrl) => {
-                  return (
-                    <div key={project.id}>
-                      <Icon icon={iconUrl} />
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
-          )}
-        </div>
-        <button onClick={closeInfo} className='btn'>
-          Close
-        </button>
+          </div>
+        )}
       </div>
-    </FocusOn>
+      <button onClick={closeInfo} className='btn'>
+        Close
+      </button>
+    </div>
   )
 }
 export default ProjectInfo
